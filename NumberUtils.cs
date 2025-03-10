c6d2d3ea5b3aef7dc1827cab2f6a1688934db2ac
MEGA <MEGA@gmail.com>
2025-03-10 12:25:49 +0200

    Added FastExponentationUsingSquares method in NumberUtils class

--- NumberUtils.cs
+++ NumberUtils.cs
@@ -5,4 +5,20 @@ public class NumberUtils
 	public NumberUtils()
 	{
 	}
+
+    public static long FastExponentationUsingSquares(long baseNum, long exp)
+    {
+        long result = 1;
+
+        while (exp > 0)
+        {
+            if ((exp & 1) == 1)
+                result *= baseNum;
+
+            baseNum *= baseNum;
+            exp >>= 1;
+        }
+
+        return result;
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+/*
 Console.WriteLine("Hello, World!");
+Console.ReadKey(true);
+*/
+
+long baseNumber = long.Parse(Console.ReadLine());
+
+long exponent = long.Parse(Console.ReadLine());
+
+long result = NumberUtils.FastExponentationUsingSquares(baseNumber, exponent);
+Console.WriteLine(result);
 Console.ReadKey(true);
