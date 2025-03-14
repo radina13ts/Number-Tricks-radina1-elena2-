7500a84e3a465ebff4dd0352c7302e494d98e424
MEGA <MEGA@gmail.com>
2025-03-14 12:14:19 +0200

    eratosten done

--- NumberUtils.cs
+++ NumberUtils.cs
@@ -1,8 +1,34 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 public class NumberUtils
 {
-	public NumberUtils()
-	{
-	}
+    public NumberUtils()
+    {
+
+    }
+    public void Eratosten(int n)
+    { 
+        List<int> non = new List<int>();
+
+        for (int i = 2; i < n; i++)
+        {
+            for (int y = i * 2; y < n; y = y + i)
+            {
+                if (non.Contains(y) == false)
+                {
+                    non.Add(y);
+                }
+            }
+        }
+        for (int z = 2; z < n; z++)
+        {
+            if (non.Contains(z) == false)
+            {
+                Console.Write(z + ", ");
+            }
+        }
+    }
+
+
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+NumberUtils test = new NumberUtils();
+Console.WriteLine("all prime nums to");
+int n = int.Parse(Console.ReadLine());
+test.Eratosten(n);
 Console.ReadKey(true);
