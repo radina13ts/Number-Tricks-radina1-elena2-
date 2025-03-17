705e32c331ba2a647b6fbf4df3bc014687f92b02
MEGA <MEGA@gmail.com>
2025-03-17 11:57:32 +0200

    Add DrawCircle

--- NumberUtils.cs
+++ NumberUtils.cs
@@ -5,4 +5,17 @@ public class NumberUtils
 	public NumberUtils()
 	{
 	}
+	public static void DrawCircle(double r)
+	{
+		int bound = (int)Math.Ceiling(r);
+		for (int y = bound - 1; y >= -bound; y--)
+		{
+			for (int x = -bound; x < bound; x++)
+			{
+				bool isInCicle = x * x + y * y < r * r;
+				Console.WriteLine(isInCicle ? "##": "   ");
+			}
+            Console.WriteLine();
+        }
+	}
 }
